1b227962f5cf7c55768985edb3a4a738e23e7598
Ubuntu <ubuntu@ip-172-31-13-251.us-west-1.compute.internal>
2016-04-24 22:49:34 +0000

    Update 4

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.put('/lions/:id', function(req, res) {
 
   var lion = _.findIndex(lions, {id: req.params.id});
   if (!lions[lion]) {
-    res.send();
+    res.send("Lion not found.");
   } else {
     var updatedLion = _.assign(lions[lion], update);
     res.json(updatedLion);
@@ -48,7 +48,7 @@ app.put('/lions/:id', function(req, res) {
 app.delete('/lions/:id',function(req,res){
 	var lion = _.findIndex(lions, {id: req.params.id});
 	if(!lions[lion]){
-	res.send();
+	res.send("Lion not found.");
 	}
 	else{
 	var deletedLion = _.assign(lions[lion]);
